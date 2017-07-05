e758354716c2fd880b06610fb54670702255c0fd
francisngo <francis.t.ngo@gmail.com>
2017-07-05 16:08:25 -0700

    add functionality for sizing in stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,52 @@
+//First In Last Out
 var Stack = function() {
   var someInstance = {};
 
+  var size = 0; //-1
+
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {}; //length = 0
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
   };
 
   someInstance.pop = function() {
+    //if there is item left in storage
+    //pop/remove the last item
+    //decrement size
+    if (size > 0) {
+      size--;
+    }
+
+
+    //otherwise do nothing
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
 };
+
+
+/*
+Object.keys(someInstance) // [].length
+if (someInstance.length <= size) {
+  size = 0;
+}
+*/
+
+/*
+
+
+
+*/
+
+//push
+//push
+//pop
+
+//pop
