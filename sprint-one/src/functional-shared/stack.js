5de1c972f614fb1ba57aab8a114eb806a4badfdb
francisngo <francis.t.ngo@gmail.com>
2017-07-06 08:49:25 -0700

    complete functional-shared stack.js functionality

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,39 +3,32 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
 
-  var storage = {};
+  someInstance.storage = {};
+  someInstance.count = 0;
 
-  var size = 0;
-
-  // someInstance.push = push;
-  // someInstance.pop = pop;
-  // someInstance.size = size;
-  extend(someInstance, Stack.stackMethods);
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
 
   return someInstance;
 };
 
-var extend = function(to, from) {
-  for (var key in from) {
-    to[key] = from[key];
+var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+stackMethods.pop = function() {
+  var itemToPop = this.storage[this.count - 1];
+  if (this.count > 0) {
+    delete this.storage[this.count -1];
+    this.count--;
   }
+  return itemToPop;
 };
 
-Stack.stackMethods = {};
-
-//push
-Stack.stackMethods.push = function(value) {
-  //do something
-  this.size++;
-};
-
-//pop
-Stack.stackMethods.pop = function() {
-  //pop item
-};
-
-//size
-Stack.stackMethods.size = function() {
-  //return size
-  return 0;
+stackMethods.size = function() {
+  return this.count;
 };
