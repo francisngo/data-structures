ec7954f84993c2d50cd77a9cf566a4436009ed98
francisngo <francis.t.ngo@gmail.com>
2017-07-05 19:54:06 -0700

    complete queue.js functionality

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,15 +7,22 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[size] = value;
     size++;
   };
 
   someInstance.dequeue = function() {
+    var itemToDequeue = storage[0];
     if (size > 0) {
+      delete storage[0];
+      for (var key in storage) {
+        storage[key - 1] = storage[key];
+      }
+      delete storage[size - 1];
       size--;
     }
+    return itemToDequeue;
   };
 
   someInstance.size = function() {
@@ -24,8 +31,3 @@ var Queue = function() {
 
   return someInstance;
 };
-
-
-//queue - First in First out Data Structure
-//push - add element to object
-//pop - remove first element from object
