778c87de35435cac97ac67f8b71077e6267b5582
francisngo <francis.t.ngo@gmail.com>
2017-07-06 08:42:40 -0700

    refactor functional queue.js variable names (size -> count)

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,32 +1,32 @@
 var Queue = function() {
   var someInstance = {};
 
-  var size = 0;
+  var count = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.enqueue = function(value) {
-    storage[size] = value;
-    size++;
+    storage[count] = value;
+    count++;
   };
 
   someInstance.dequeue = function() {
     var itemToDequeue = storage[0];
-    if (size > 0) {
+    if (count > 0) {
       delete storage[0];
       for (var key in storage) {
         storage[key - 1] = storage[key];
       }
-      delete storage[size - 1];
-      size--;
+      delete storage[count - 1];
+      count--;
     }
     return itemToDequeue;
   };
 
   someInstance.size = function() {
-    return size;
+    return count;
   };
 
   return someInstance;
