5c58457a83114d0cb1b89e70bc5e5bd390413a0d
francisngo <francis.t.ngo@gmail.com>
2017-07-05 19:09:02 -0700

    add size functionality for queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,6 +1,7 @@
 var Queue = function() {
   var someInstance = {};
 
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -8,15 +9,23 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
-
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+    }
   };
 
   someInstance.size = function() {
-
+    return size;
   };
 
   return someInstance;
 };
+
+
+//queue - First in First out Data Structure
+//push - add element to object
+//pop - remove first element from object
