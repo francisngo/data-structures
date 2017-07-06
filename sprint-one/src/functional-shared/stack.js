6beb900213ac6001f2e82d8c4015a15a8b3fc88f
francisngo <francis.t.ngo@gmail.com>
2017-07-05 22:26:52 -0700

    create functional-shared stack.js skeleton

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  var storage = {};
+
+  var size = 0;
+
+  // someInstance.push = push;
+  // someInstance.pop = pop;
+  // someInstance.size = size;
+  extend(someInstance, Stack.stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
+Stack.stackMethods = {};
 
+//push
+Stack.stackMethods.push = function(value) {
+  //do something
+  this.size++;
+};
+
+//pop
+Stack.stackMethods.pop = function() {
+  //pop item
+};
+
+//size
+Stack.stackMethods.size = function() {
+  //return size
+  return 0;
+};
