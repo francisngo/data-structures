a5eeae58117d1e179a05b5bddfd021c3dda6ee83
francisngo <francis.t.ngo@gmail.com>
2017-07-07 12:51:29 -0700

    complete tree functionality

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,9 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,12 +13,36 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children.push(newChild);
 };
 
 treeMethods.contains = function(target) {
+  //default is false (not found);
+  var result = false;
+  // create a recursion function to search tree
+  var searchTree = function(tree) {
+    //if the current tree.value equals target
+    if (tree.value === target) {
+      //assign result to true
+      result = true;
+    //otherwise if tree.value isn't the target
+    } else {
+      //and if the there are children
+      if (tree.children.length) {
+        //iterate through the children tree
+        tree.children.forEach(function(childTree) {
+          //call searchTree on each child tree.
+          searchTree(childTree);
+        });
+      }
+    }
+  };
+  //invoke searchTree function on the tree object
+  searchTree(this);
+  //return the result
+  return result;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
