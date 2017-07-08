0a32ff15042a15286b2a1da592b5e34374550143
francisngo <francis.t.ngo@gmail.com>
2017-07-08 08:28:22 -0700

    clean insert and retrieve comments for hashTable.js

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,47 +1,42 @@
 var HashTable = function() {
   this._limit = 8;
-  //an array with a limit. the limit is 8
+  // an array with a limit. the limit is 8
   this._storage = LimitedArray(this._limit); //[]
 };
 
 HashTable.prototype.insert = function(k, v) {
-  //hash function takes in the given key and limit
-  //converts index to 32-bit integer
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  // hash function takes in the given key and limit
+  // converts index to 32-bit integer
 
   // create a bucket array to store the key/value
   // if the key index already exists, go with that bucket, otherwise, start another bucket array
-  var bucket = this._storage.get(index) || [];
   // push the new key value to the bucket array
+  // set the object's storage with the index assigned to the bucket
+
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index) || [];
   bucket.push([k, v]);
-  //set the object's storage with the index assigned to the bucket
   this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  //hash function that converts key into index number
-  var index = getIndexBelowMaxForKey(k, this._limit);
-
-  //find the index in the storage
-  var bucket = this._storage.get(index);
-
+  // hash function that converts key into index number
+  // find the index in the storage
   // if there is not bucket match return null
-  if (!bucket) {
-    return null;
-  }
-
   // iterate through each tuple in the bucket
     //if tuple's key matches given key
       //return the tuple's value
     //otherwise return null;
 
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
   for (var i = 0; i < bucket.length; i++) {
     if (bucket[i][0] === k) {
       return bucket[i][1];
     }
   }
-
   return null;
+  
 };
 
 HashTable.prototype.remove = function(k) {
