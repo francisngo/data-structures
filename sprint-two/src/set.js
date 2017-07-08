f6309e5f4a36e388a73364d4e304a58dc3479468
francisngo <francis.t.ngo@gmail.com>
2017-07-07 19:47:23 -0700

    complete set.js functionality

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //add item to storage object
+  //set property of storage to item
+  //set value of that property to item
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  //search storage for property that equals item
+  //if the item is found, return true
+  //otherwise return false
+  var result = false;
+  if (this._storage.hasOwnProperty(item)) {
+    result = true;
+  }
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  //delete value at the [item] property
+  delete this._storage[item];
 };
 
 /*
